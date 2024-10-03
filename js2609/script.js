df546bd2ee18af9bd7185739388844da89157b27
Aysla <aysladev@gmail.com>
2024-10-03 15:04:29 -0300

    Add files via upload

--- js2609/script.js
+++ js2609/script.js
@@ -1,14 +1,16 @@
-var quantas = parseFloat (prompt("Quantas cartas deseja virar?"));
-for (let i = 0; i < quantas; i++){
-var naipes = Math.floor(Math.random() * 4);
-var valor = Math.floor(Math.random() * 10) + 1;
-if(naipes == 0){
-    alert(valor + ' paus');
-}else if(naipes == 1){
-    alert(valor + ' copas');
-}else if(naipes == 2){
-    alert(valor + ' espadas');
-}else if(naipes == 3){
-    alert(valor + ' ouros');
+let notas = [], nomes = [], media, total = 0;
+for(let i = 1; i <=5; i++){
+    nota = parseFloat(prompt("Digite sua nota: "));
+    nome = prompt("Digite seu nome: ");
+    notas.push(nota);
+    nomes.push(nome);
+    total += nota;
 }
+media = total / 5
+console.log(notas, nomes, media);
+console.log("Alunos abaixo da média: ")
+for(let i = 0; i < notas.length; i++){ //começa no 0 pq é o 'length', loop pra mostrar as notas abaixp da media
+    if(notas[i] < media){ //importante o i, tanto no if quanto no console log (praq? nao vou lembrar)
+        console.log(`Aluno: ${nomes[i]} Nota: ${notas[i]}`);
+    }
 }
